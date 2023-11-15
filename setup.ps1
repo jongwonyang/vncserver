652e18e6f22e5e5e9872384b417f701060d42b7d
Mitchell <yjw963@gmail.com>
2023-11-15 14:01:11 +0900

    add websockify

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,12 @@
 $TIGHTVNC_URL = "https://www.tightvnc.com/download/2.8.81/tightvnc-2.8.81-gpl-setup-64bit.msi"
+$WEBSOCKIFY_URL = "https://github.com/novnc/websockify/archive/refs/tags/v0.11.0.zip"
 $TIGHTVNC_PATH = ".\TightVNC.msi"
+$WEBSOCKIFY_PATH = ".\websokify.zip"
+$WEBSOCKIFY_DEST = ".\websockify"
 
 Invoke-WebRequest -Uri $TIGHTVNC_URL -OutFile $TIGHTVNC_PATH
+Invoke-WebRequest -Uri $WEBSOCKIFY_URL -OutFile $WEBSOCKIFY_PATH
 
-Start-Process msiexec -ArgumentList "/i $TIGHTVNC_PATH /qn" -Wait
+Expand-Archive -Path $WEBSOCKIFY_PATH -DestinationPath $WEBSOCKIFY_DEST
+
+Start-Process -FilePath $TIGHTVNC_PATH -Wait
