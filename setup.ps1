49e6b63da47a5fa934f7227f1e7005dc154c6d50
Mitchell <yjw963@gmail.com>
2023-11-15 13:39:23 +0900

    update download path

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,5 @@
 $TIGHTVNC_URL = "https://www.tightvnc.com/download/2.8.81/tightvnc-2.8.81-gpl-setup-64bit.msi"
-$TIGHTVNC_PATH = "C:\Temp\TightVNC.msi"
+$TIGHTVNC_PATH = ".\TightVNC.msi"
 
 Invoke-WebRequest -Uri $TIGHTVNC_URL -OutFile $TIGHTVNC_PATH
 
