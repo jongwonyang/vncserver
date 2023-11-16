51846cfc4563f5c13b350c1ac5a11b8b51747d6b
Mitchell <yjw963@gmail.com>
2023-11-16 16:46:46 +0900

    change to config

--- config.ps1
+++ config.ps1
@@ -12,20 +12,34 @@ $PYTHON_PATH = "$TEMP_PATH\python-3.12.0.exe"
 New-Item $TEMP_PATH -ItemType Directory -Force
 
 # Download
+Write-Host "Downloading TightVNC... " -NoNewline
 Invoke-WebRequest -Uri $TIGHTVNC_URL -OutFile $TIGHTVNC_PATH
+Write-Host "Done"
+Write-Host "Downloading Websockify... " -NoNewline
 Invoke-WebRequest -Uri $WEBSOCKIFY_URL -OutFile $WEBSOCKIFY_PATH
+Write-Host "Done"
+Write-Host "Downloading Python... " -NoNewline
 Invoke-WebRequest -Uri $PYTHON_URL -OutFile $PYTHON_PATH
+Write-Host "Done"
 
 # Install TightVNC
+Write-Host "Installing TightVNC (Please follow direction)... " -NoNewline
 Start-Process -FilePath $TIGHTVNC_PATH -Wait
+Write-Host "Done"
 
 # Install python
+Write-Host "Installing Python (Please follow direction)... " -NoNewline
 Start-Process -FilePath $PYTHON_PATH -Wait
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+Write-Host "Done"
 
 # Install websockify
+Write-Host "Installing Websockify... " -NoNewline
 Expand-Archive -Path $WEBSOCKIFY_PATH -DestinationPath $WEBSOCKIFY_DEST -Force
 Set-Location ".\$WEBSOCKIFY_DEST\websockify-0.11.0"
 & python -m pip install setuptools
 & python setup.py install
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+Write-Host "Done"
+
+Write-Host "Setup was successful."
