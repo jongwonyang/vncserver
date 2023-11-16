3ca862ec31f1bfe1a15d874932c79a9d5bece0b7
Mitchell <yjw963@gmail.com>
2023-11-16 13:13:52 +0900

    update env var

--- setup.ps1
+++ setup.ps1
@@ -2,13 +2,14 @@ $TIGHTVNC_URL = "https://www.tightvnc.com/download/2.8.81/tightvnc-2.8.81-gpl-se
 $WEBSOCKIFY_URL = "https://github.com/novnc/websockify/archive/refs/tags/v0.11.0.zip"
 $PYTHON_URL = "https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe"
 
-$TIGHTVNC_PATH = ".\temp\TightVNC.msi"
-$WEBSOCKIFY_PATH = ".\temp\websokify.zip"
-$WEBSOCKIFY_DEST = ".\temp\websockify"
-$PYTHON_PATH = ".\temp\python-3.12.0.exe"
+$TEMP_PATH = ".\temp"
+$TIGHTVNC_PATH = "$TEMP_PATH\TightVNC.msi"
+$WEBSOCKIFY_PATH = "$TEMP_PATH\websokify.zip"
+$WEBSOCKIFY_DEST = "$TEMP_PATH\websockify"
+$PYTHON_PATH = "$TEMP_PATH\python-3.12.0.exe"
 
 # Make temp directory
-New-Item temp -ItemType Directory -Force
+New-Item $TEMP_PATH -ItemType Directory -Force
 
 # Download
 Invoke-WebRequest -Uri $TIGHTVNC_URL -OutFile $TIGHTVNC_PATH
@@ -20,9 +21,10 @@ Start-Process -FilePath $TIGHTVNC_PATH -Wait
 
 # Install python
 Start-Process -FilePath $PYTHON_PATH -Wait
+[Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Process)
 
 # Install websockify
 Expand-Archive -Path $WEBSOCKIFY_PATH -DestinationPath $WEBSOCKIFY_DEST -Force
-& python -m pip install setuptools
 Set-Location ".\$WEBSOCKIFY_DEST\websockify-0.11.0"
+& python -m pip install setuptools
 & python setup.py install
